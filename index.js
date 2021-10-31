c90e23efe75eae11bdc3c32e3bfbccfec89b8080
hrid620 <hridishere@gmail.com>
2021-10-31 23:31:19 +0600

    done server

--- index.js
+++ index.js
@@ -51,9 +51,18 @@ async function run() {
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
+            const result = await orderCollection.insertOne(query);
+            res.json(result);
+        })
+
+        // approved
+        app.post('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.json(result);
         })
+
 
         // get manageAllOrder api
         app.get('/manageAllOrders', async (req, res) => {
